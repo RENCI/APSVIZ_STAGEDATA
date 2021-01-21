c84ba3d2e9da7903983122267d0ab3a135f741ce
Jeffrey Tilson <jtilson@renci.org>
2021-01-21 09:21:58 -0500

    Initial code

---
+++ stage_data.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os, sys, wget
+import logging
+
+filelist={'zeta_max':    'maxele.63.nc', 
+          'swan_HS_max': 'swan_HS_max.63.nc',
+          'wind_max':    'maxwvel.63.nc'}
+mode = 0o755
+
+def getDataFile(outdir, url, infilename):
+    '''
+    '''
+    # Get infilename and download netcdf file
+    outfilename = wget.download(os.path.join(url,infilename), os.path.join(outdir,infilename))
+    return outfilename
+
+
+def main(args):
+    '''    
+    '''
+    logging.basicConfig(filename='log',format='%(asctime)s : %(levelname)s : %(funcName)s : %(module)s : %(name)s : %(message)s', level=logging.DEBUG)
+    # process args
+    if not args.inputURL:
+        print(f"Need url on command line: --url <url>.")
+        return 1
+    inputURL = args.inputURL.strip()
+
+    if not args.outputDir:
+        print(f"Need output directory on command line: --output <outputdir>.")
+        return 1
+
+    if not os.path.exists(args.outputDir):
+        if args.verbose: print(f"Mkdir-ing {args.output}")
+        os.makedirs(args.outputDir)
+
+    logging.info('Input URL is {}'.format(inputURL))
+    logging.info('OutputDir is {}'.format(args.outputDir))
+
+    num = len(filelist)
+    for v in filelist: 
+        if args.verbose: print(f"Getting {v}")
+        outname=getDataFile(args.outputDir, inputURL, filelist[v])
+        print(f"")
+    logging.info('Finished moving {} files '.format(num))
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=main.__doc__)
+    parser.add_argument('--verbose', help="Turn on debug statements", action='store_true')
+    parser.add_argument('--inputURL', default=None, help='URL to retrieve data from', type=str)
+    parser.add_argument('--outputDir', default=None, help='Destination directory', type=str)
+    
+    args = parser.parse_args()
+
+    sys.exit(main(args))
+
+
