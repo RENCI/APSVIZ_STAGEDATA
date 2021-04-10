60a61fafb616c0b2f7b44b4c2fea8743dcbf9b6b
Lisa Stillwell <lisa@renci.org>
2021-04-09 20:05:04 -0400

    still more sytax problems

--- load-geoserver-images.py
+++ load-geoserver-images.py
@@ -48,6 +48,9 @@ class asgsDB:
     def get_port(self):
         return self.port
 
+    def getdbname(self):
+        return self.db_name
+
 
     # given instance id - save geoserver url (to access this mbtiles layer) in the asgs database
     def saveImageURL(self, instanceId, name, url):
@@ -120,7 +123,7 @@ class asgsDB:
                     logger.debug(f"opened csv file - saving this row to db: {row}")
                     png_url = f"https://{geoserver_host}/obs_pngs/{instance_id}/{row[6]}"
                     cursor.execute(
-                        "INSERT INTO users VALUES (%s, %s, %s, %s %s %s %s ST_SetSRID(ST_MakePoint(%s, %s),4326)) %s %s)",
+                        "INSERT INTO stations(stationid, stationname, state, lat, lon, node, filename, the_geom, instanceid, image_url) VALUES (%s, %s, %s, %s %s %s %s ST_SetSRID(ST_MakePoint(%s, %s),4326)) %s %s)",
                         row, row[4], row[3], instance_id, png_url
                     )
             self.conn.commit()
